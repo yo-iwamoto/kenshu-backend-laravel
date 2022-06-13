a6aec0bc72ae5b6d051e329f9a37a073da09e37c
you-5805 <you.iwamoto@prtimes.co.jp>
2022-06-13 09:46:34 +0900

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => view('root'));
+
+Route::get('/signup', [UserController::class, 'create']);
+Route::resource('/user', UserController::class)->only([
+    'store'
+]);
+
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::resource('/session', SessionController::class)->only([
+    'store',
+    'destroy',
+]);
