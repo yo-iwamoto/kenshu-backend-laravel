a6d2e5009b4429e754fa60ba2c77c6d041de76c6
you-5805 <you.iwamoto@prtimes.co.jp>
2022-06-14 15:55:58 +0900

    fix: factory typo

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'profile_image_url' => '/img/default-icn.png',
+            'profile_image_url' => '/img/default-icon.png',
             'remember_token' => Str::random(10),
         ];
     }
