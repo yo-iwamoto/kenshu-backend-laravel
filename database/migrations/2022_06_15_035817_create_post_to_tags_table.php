b17563b7b076d41196eb9904f3242423f9d00cc2
you-5805 <you.iwamoto@prtimes.co.jp>
2022-06-15 13:32:38 +0900

    migration: create tags, post_to_tags table

---
+++ database/migrations/2022_06_15_035817_create_post_to_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_to_tags', function (Blueprint $table) {
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->primary(['post_id', 'tag_id']);
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_to_tags');
+    }
+};
