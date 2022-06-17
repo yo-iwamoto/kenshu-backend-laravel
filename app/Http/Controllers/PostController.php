76a1dc9e97774bd074e8b2ee96234ba1c00f7930
you-5805 <you.iwamoto@prtimes.co.jp>
2022-06-17 14:08:10 +0900

    impl: error feedback

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,9 @@ use App\Services\IndexTagServiceInterface;
 use App\Services\ShowPostServiceInterface;
 use App\Services\StorePostServiceInterface;
 use App\Services\UpdatePostServiceInterface;
+use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Validation\UnauthorizedException;
 
@@ -45,36 +47,67 @@ class PostController extends Controller
         StorePostRequest $request,
         StorePostServiceInterface $store_post_service,
     ) {
-        $form_data = $request->validated();
+        try {
+            // ゲストユーザーのエラー処理
+            if ($request->user() === null) {
+                return redirect('/')->with([
+                'message' => 'ゲストユーザーは記事を投稿できません。新規会員登録またはログインしてください',
+            ]);
+            }
 
-        $store_post_service->execute(
-            user_id: $request->user()->id,
-            title: $form_data['title'],
-            content: $form_data['content'],
-            thumbnail_image_index: $form_data['thumbnail_image_index'],
-            images: $request->file('images'),
-            tag_ids: isset($form_data['tags']) ? $form_data['tags'] : null,
-        );
+            $form_data = $request->validated();
 
-        return redirect('/');
+            $store_post_service->execute(
+                user_id: $request->user()->id,
+                title: $form_data['title'],
+                content: $form_data['content'],
+                thumbnail_image_index: $form_data['thumbnail_image_index'],
+                images: $request->file('images'),
+                tag_ids: isset($form_data['tags']) ? $form_data['tags'] : null,
+            );
+
+            return redirect('/');
+        } catch (Exception $e) {
+            return redirect('/')->with([
+                'message' => '記事の作成に失敗しました',
+            ]);
+        }
     }
 
 
     public function edit(
+        Request $request,
         ShowPostServiceInterface $show_post_service,
         IndexTagServiceInterface $index_tag_service,
         string $post_id,
     ) {
-        $post = $show_post_service->execute($post_id);
-        $all_tags = $index_tag_service->execute();
+        try {
+            // ゲストユーザーのエラー処理
+            // 記事の取得処理が省略可能なため、他のユーザーの場合と分けて先に行なっている
+            if ($request->user() === null) {
+                throw new UnauthorizedException();
+            }
 
-        $related_tag_ids = $post->tags()->getResults()->pluck('id')->toArray();
+            $post = $show_post_service->execute($post_id);
 
-        return view('post.edit', [
-            'post' => $post,
-            'tags' => $all_tags,
-            'tag_ids' => $related_tag_ids,
-        ]);
+            // 他のユーザーのエラー処理
+            if ($post->user->id !== $request->user()->id) {
+                throw new UnauthorizedException();
+            }
+
+            $all_tags = $index_tag_service->execute();
+            $related_tag_ids = $post->tags()->getResults()->pluck('id')->toArray();
+
+            return view('post.edit', [
+                'post' => $post,
+                'tags' => $all_tags,
+                'tag_ids' => $related_tag_ids,
+            ]);
+        } catch (UnauthorizedException $e) {
+            return redirect('/')->with([
+                'message' => '他のユーザーの記事を編集することはできません',
+            ]);
+        }
     }
 
     public function update(
@@ -82,19 +115,25 @@ class PostController extends Controller
         UpdatePostServiceInterface $update_post_service,
         string $post_id,
     ) {
-        $form_data = $request->validated();
+        try {
+            $form_data = $request->validated();
 
-        $update_post_service->execute(
-            post_id: $post_id,
-            user_id: $request->user()->id,
-            title: $form_data['title'],
-            content: $form_data['content'],
-            thumbnail_image_index: $form_data['thumbnail_image_index'],
-            images: $request->file('images'),
-            tag_ids: isset($form_data['tags']) ? $form_data['tags'] : null,
-        );
+            $update_post_service->execute(
+                post_id: $post_id,
+                user_id: $request->user()->id,
+                title: $form_data['title'],
+                content: $form_data['content'],
+                thumbnail_image_index: $form_data['thumbnail_image_index'],
+                images: $request->file('images'),
+                tag_ids: isset($form_data['tags']) ? $form_data['tags'] : null,
+            );
 
-        return redirect(route('post.show', ['post' => $post_id]));
+            return redirect(route('post.show', ['post' => $post_id]));
+        } catch (Exception $e) {
+            return redirect(route('post.show', ['post' => $post_id]))->with([
+                'message' => '更新処理に失敗しました',
+            ]);
+        }
     }
 
     public function destroy(
