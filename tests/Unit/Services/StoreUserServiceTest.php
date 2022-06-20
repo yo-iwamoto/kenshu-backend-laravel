ead7ed906829f10258b41bfa31b20c66d675a445
you-5805 <you.iwamoto@prtimes.co.jp>
2022-06-20 15:56:12 +0900

    add mock file in testing

--- tests/Unit/Services/StoreUserServiceTest.php
+++ tests/Unit/Services/StoreUserServiceTest.php
@@ -5,31 +5,35 @@ namespace Tests\Unit;
 use App\Models\User;
 use App\Services\StoreUserService;
 use App\Services\StoreUserServiceInterface;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class StoreUserServiceTest extends TestCase
 {
     private StoreUserServiceInterface $service;
+    private string $testImagePath;
 
     public function setUp(): void
     {
         parent::setUp();
 
         $this->service = app(StoreUserService::class);
+        $this->testImagePath = base_path('/tests/tmpFiles/test.png');
     }
 
     /**
-     * ファイルのアップロードとセッションに関心を持たない
+     * セッションに関心を持たない
      */
-    public function test_ファイル無しの正しい入力でUserが作成されること(): void
+    public function test_正しい入力でUserが作成されること(): void
     {
         $email = $this->faker->email();
+        $file = new UploadedFile($this->testImagePath, basename($this->testImagePath));
 
         $this->service->execute(
             name: $this->faker->name(),
             email: $email,
             password: $this->faker->password(),
-            file: null,
+            file: $file,
         );
 
         $data = User::where('email', $email)->get()->toArray();
